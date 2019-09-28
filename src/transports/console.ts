ed1ee9c74c4124e78101c59663091eb8f76aa822
sashko <7355623+a-pavlenko@users.noreply.github.com>
2019-09-28 12:39:20 +0300

    remove unused colors

--- src/transports/console.ts
+++ src/transports/console.ts
@@ -12,12 +12,9 @@ const colors: Colors = {
   reset: '\x1b[0m',
   dim: '\x1b[2m',
   red: '\x1b[31m',
-  green: '\x1b[32m',
   yellow: '\x1b[33m',
   blue: '\x1b[34m',
-  magenta: '\x1b[35m',
   cyan: '\x1b[36m',
-  white: '\x1b[37m',
 };
 
 const colorsLevelMap: ColorsLevelMap = {
